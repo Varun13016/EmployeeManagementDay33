a4c746731be2d92bd7b534511942579f7bdeb0f2
“Varun13016” <dmevarun@gmail.com>
2021-12-12 21:28:04 +0530

    Added-UC1

--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -1,4 +1,4 @@
-﻿using EmployeeManagment;
+﻿using EmployeeManagement;
 using System;
 
 namespace EmployeeManagement
@@ -11,32 +11,32 @@ namespace EmployeeManagement
             EmployeeRepo repo = new EmployeeRepo();
             EmployeeModel employee = new EmployeeModel();
 
-            //employee.EmployeeName = "Varun13016";
-            //employee.PhoneNumber = "87754465546";
-            //employee.Address = "Banglore";
-            //employee.Department = "PROD.Manager";
-            //employee.Gender = 'M';
-            //employee.BasicPay = 800563;
-            //employee.Deductions = 212;
-            //employee.TaxablePay = 454;
-            //employee.Tax = 465;
-            //employee.NetPay = 25898;
-            //employee.City = "Banglore";
-            //employee.Country = "India";
-            //employee.StartDate = DateTime.Now;
+            employee.EmployeeName = "Varun13016";
+            employee.PhoneNumber = "87754465546";
+            employee.Address = "Banglore";
+            employee.Department = "PROD.Manager";
+            employee.Gender = 'M';
+            employee.BasicPay = 800563;
+            employee.Deductions = 212;
+            employee.TaxablePay = 454;
+            employee.Tax = 465;
+            employee.NetPay = 25898;
+            employee.City = "Banglore";
+            employee.Country = "India";
+            employee.StartDate = DateTime.Now;
 
-            ////repo.AddEmployee(employee);
-            //repo.GetAllEmployee();
+            repo.AddEmployee(employee);
+            repo.GetAllEmployee();
 
-            Console.WriteLine("Hello World!");
-            Salary salary = new Salary();
-            SalaryUpdateModel salaryUpdateModel = new SalaryUpdateModel();
-            salaryUpdateModel.EmployeeSalary = 1300;
-            salaryUpdateModel.SalaryId = 1;
-            salaryUpdateModel.EmployeeId = 1;
-            salaryUpdateModel.Month = "Jan";
+            //Console.WriteLine("Hello World!");
+            //Salary salary = new Salary();
+            //SalaryUpdateModel salaryUpdateModel = new SalaryUpdateModel();
+            //salaryUpdateModel.EmployeeSalary = 1300;
+            //salaryUpdateModel.SalaryId = 1;
+            //salaryUpdateModel.EmployeeId = 1;
+            //salaryUpdateModel.Month = "Jan";
 
-            salary.UpdateEmployeeSalary(salaryUpdateModel);
+            //salary.UpdateEmployeeSalary(salaryUpdateModel);
         }
     }
 }
